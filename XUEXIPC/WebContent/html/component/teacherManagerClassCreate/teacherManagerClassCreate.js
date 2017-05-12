2ba3309f667ddbdda907ee8348738782f9640a2d
betterwmj <happytuw@163.com>
2017-05-12 11:33:10 +0800

    创建班级中加入删除班级功能，判断班级名是否为空

--- XUEXIPC/WebContent/html/component/teacherManagerClassCreate/teacherManagerClassCreate.js
+++ XUEXIPC/WebContent/html/component/teacherManagerClassCreate/teacherManagerClassCreate.js
@@ -27,22 +27,45 @@ function controller($scope,$element,$state,$cookies,http){
     }
   }
   vm.create = async function(){
-    try {
-      let result = await http.get("CreateClass",{
-        className:vm.classInfo.className,
-        subjectID:vm.currentSubject.SubjectID
-      });
-      if( result === true ){
-        showErrMsg("班级创建成功");
-        let classes = await http.get("GetTeacherClasses");
-  	    vm.classes = classes;
-      }else{
-        showErrMsg("该班级已存在");
-      }
-    } catch (error) {
-      showErrMsg("班级创建失败");
-    }
+      console.log(vm.classInfo.className);
+	  if( checkClass(vm.classInfo.className) ===true){
+    	 try {
+    	      let result = await http.get("CreateClass",{
+    	        className:vm.classInfo.className,
+    	        subjectID:vm.currentSubject.SubjectID
+    	      });
+    	      if( result === true ){
+    	        showErrMsg("班级创建成功");
+    	        let classes = await http.get("GetTeacherClasses");
+    	  	    vm.classes = classes;
+    	      }else{
+    	        showErrMsg("该班级已存在");
+    	      }
+    	    } catch (error) {
+    	      showErrMsg("班级创建失败");
+    	    }
+     }else{
+    	 showErrMsg("班级名不能为空！");
+     }
   }
+  vm.deleteClass =async function(classId){
+	     let confirm =await showConfrimMsg("是否删除该班级?");
+	     if(confirm ==="确定"){
+	    	 let userInfo = $cookies.getObject("userInfo");
+		     let result = await http.get("DeleteTeacherClass",{
+		    	 teacherId:userInfo.id,
+		    	 classId:classId
+		      });
+		      if( result === true ){
+		        showErrMsg("班级删除成功");
+		        let classes = await http.get("GetTeacherClasses");
+		  	    vm.classes = classes;
+		      }else{
+		        showErrMsg("删除失败");
+		      }
+	     }    
+  }
+  
   function showErrMsg(errMsg) {
 	     
       dialog.openDialog({
@@ -51,5 +74,20 @@ function controller($scope,$element,$state,$cookies,http){
           content: errMsg
       });
     
+  }
+  async function showConfrimMsg(confirmMsg) {
+      return await dialog.openDialog({
+          type: "normal",
+          title: "操作提示",
+          content: confirmMsg
+      });
+  }
+  function checkClass(className){
+	  if(className === null || className ===undefined ||className ==="" ){ 
+	    	 return false;
+	   }  else{
+		    return true;
+	   }
+	  
   }
 }
